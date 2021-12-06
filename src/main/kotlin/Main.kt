54ab734b3172785178cb225f012d48e1fd5a2210
TungNguyen <tung.nk41@gmail.com>
2021-12-06 20:16:49 +0700

    2021 day 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
-import y2020.*
+import y2021.*
 
 fun main(args: Array<String>) {
 
-    val problem = Day02()
+    val problem = Day01()
     problem.solve()
 
 }
--- src/main/kotlin/Problem.kt
+++ src/main/kotlin/Problem.kt
@@ -1,8 +1,13 @@
 abstract class Problem(val day: Int,val year: Int) {
+    var ansPart1 = 0
+    var ansPart2 = 0
     fun solve(){
         partOne()
         partTwo()
+        println("Part 1: ${ansPart1}")
+        println("Part 2: ${ansPart2}")
     }
     abstract fun partOne()
     abstract fun partTwo()
+
 }
